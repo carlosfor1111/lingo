39d746804a03c44a82bf7fb0e3f01944182c2103
carloslin <carloslin@phalanxbiotech.com>
2024-03-27 10:45:57 +0800

    feat:add page Wrapper

---
+++ app/(main)/learn/header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+type Props = {
+  title: string;
+};
+
+export const Header = ({ title }: Props) => {
+  return (
+    <div className="sticky top-0 bg-white pb-3 lg:pt-[28px] lg:mt-[-28px] flex items-center justify-between border-b-2 text-neutral-400 lg:z-50 ">
+      <Link href="/courses">
+        <Button>
+          <ArrowLeft className="w-5 h-5 stoke-2 text-neutral-400" />
+        </Button>
+        <h1 className="font-bold text-lg">{title}</h1>
+      </Link>
+      <div />
+    </div>
+  );
+};
