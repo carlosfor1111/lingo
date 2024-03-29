66e29056fcf8a52ba22cf1f0e5cd35937f0a9cd0
carloslin <carloslin@phalanxbiotech.com>
2024-03-29 11:38:20 +0800

    feat:add lesson Button

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,16 +2,36 @@ import { redirect } from "next/navigation";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { UserProgress } from "@/components/user-progress";
+import { lessons, units as unitsSchema } from "@/db/schema";
 import { Header } from "@/app/(main)/learn/header";
-import { getUserProgress } from "@/db/queries";
+import { Unit } from "@/app/(main)/learn/unit";
+
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+} from "@/db/queries";
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
+  const unitsData = getUnits();
+  const lessonPercentageData = getLessonPercentage();
+  const courseProgressData = getCourseProgress();
 
-  const [userProgress] = await Promise.all([userProgressData]);
+  const [userProgress, units, courseProgress, lessonsPercentage] =
+    await Promise.all([
+      userProgressData,
+      unitsData,
+      courseProgressData,
+      lessonPercentageData,
+    ]);
 
   if (!userProgress || !userProgress.activeCourse) redirect("/courses");
 
+  if (!courseProgress) {
+    redirect("/courses");
+  }
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
@@ -24,6 +44,25 @@ const LearnPage = async () => {
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={
+                courseProgress.activeLesson as
+                  | (typeof lessons.$inferSelect & {
+                      unit: typeof unitsSchema.$inferSelect;
+                    })
+                  | undefined
+              }
+              activeLessonPercentage={lessonsPercentage}
+            />
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
