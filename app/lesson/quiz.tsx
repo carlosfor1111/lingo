f50f3272e4b3d9833c8caa51739ce833041635cc
carloslin <carloslin@phalanxbiotech.com>
2024-04-01 09:03:17 +0800

    feat: add lesson and question

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -9,7 +9,9 @@ import { useAudio, useWindowSize, useMount } from "react-use";
 
 import { Footer } from "@/app/lesson/footer";
 import { ResultCard } from "@/app/lesson/result-card";
-import { Card } from "@/app/lesson/card";
+import { Header } from "@/app/lesson/header";
+import { Challenge } from "@/app/lesson/challenge";
+import { QuestionBubble } from "@/app/lesson/question-bubble";
 
 import { reduceHearts } from "@/actions/user-progress";
 import { upsertChallengeProgress } from "@/actions/challenge-progress";
@@ -204,12 +206,46 @@ export const Quiz = ({
     );
   }
 
+  const title =
+    challenge.type === "ASSIST"
+      ? "Select the correct meaning"
+      : challenge.question;
+
   return (
     <>
+      {incorrectAudio}
+      {correctAudio}
       <Header
         hearts={hearts}
         percentage={percentage}
         hasActiveSubscription={!!userSubscription?.isActive}
+      />
+      <div className="flex-1">
+        <div className="h-full flex items-center justify-center">
+          <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
+            <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
+              {title}
+            </h1>
+            <div>
+              {challenge.type === "ASSIST" && (
+                <QuestionBubble question={challenge.question} />
+              )}
+              <Challenge
+                options={options}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
+                disabled={pending}
+                type={challenge.type}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer
+        disabled={pending || !selectedOption}
+        status={status}
+        onCheck={onContinue}
       />
     </>
   );
--- app/lesson/result-card.tsx
+++ app/lesson/result-card.tsx
@@ -18,7 +18,32 @@ export const ResultCard = ({ value, variant }: Props) => {
         variant === "hearts" && "bg-rose-400 border-rose-400"
       )}
     >
-      
+      <div
+        className={cn(
+          "p-1.5 text-white rounded-t-xl font-bold text-center uppercase text-xs",
+          variant === "hearts" && "bg-rose-500",
+          variant === "points" && "bg-orange-400"
+        )}
+      >
+        {variant === "hearts" ? "Hearts Left" : "Total XP"}
+      </div>
+
+      <div
+        className={cn(
+          "rounded-2xl bg-white items-center flex justify-center p-6 font-bold text-lg",
+          variant === "hearts" && "bg-rose-500",
+          variant === "points" && "bg-orange-400"
+        )}
+      >
+        <Image
+          alt="Icon"
+          src={imageSrc}
+          height={30}
+          width={30}
+          className="mr-1.5"
+        />
+        {value}
+      </div>
     </div>
   );
 };
